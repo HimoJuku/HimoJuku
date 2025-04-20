51e68ffef6f9b5bd813c3ea8e2c83f4937dd503b
Zeppelin_K <littlejun3004@outlook.com>
2025-04-20 23:39:58 +0800

    fix: refactor drawer layout code, improve drawer style

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -42,14 +42,23 @@ type DrawerParamList = {
   // ...etc
 };
 
-import { lightColors, darkColors } from '../constants/Colors';
-
 SplashScreen.preventAutoHideAsync();
 
-// 创建Drawer
+// Initialize the Drawer
 const Drawer = createDrawerNavigator<DrawerParamList>();
-
-
+// Initialize the ColorScheme
+const colorScheme = useColorScheme();
+const paperTheme =
+  colorScheme === 'dark'
+    ? { ...MD3DarkTheme, colors: Colors.dark.colors}
+    : { ...MD3LightTheme, colors: Colors.light.colors };
+//Calculate the width of the drawer based on the screen size
+const drawerWidth =
+(
+  (Dimensions.get('window').width<Dimensions.get('window').height)
+  ? Dimensions.get('window').width
+  : Dimensions.get('window').height
+)* 8 / 18 + 20; // 这里调侧栏的大小
 
 function Header() {
   const navigation = useNavigation<DrawerNavigationProp<DrawerParamList>>();
@@ -65,21 +74,110 @@ function Header() {
   );
 }
 
+
+function LeftDrawer(){
+  return (
+  <Drawer.Navigator
+    initialRouteName="bookShelf"
+    drawerContent={(props) => <CustomDrawerContent {...props} />}
+    screenOptions={{
+      drawerActiveTintColor: paperTheme.colors.onPrimary,
+      drawerActiveBackgroundColor: paperTheme.colors.primary,
+      drawerInactiveTintColor: paperTheme.colors.onSurfaceVariant,
+      drawerStyle: {
+        // Set the width of the drawer
+        width: drawerWidth,
+        backgroundColor: paperTheme.colors.primaryContainer,
+      },
+      drawerItemStyle: {
+        width: '100%',
+        borderRadius: 80,
+        paddingEnd: 10,
+        paddingStart: 10,
+      },
+      header: () => <Header />, //Override the header that could be turned off in the screen options
+    }}
+  >
+    {/* 选项1：书架，对应 app/(tabs) 目录或文件 */}
+    <Drawer.Screen
+      name="bookShelf"
+      options={{
+        title: 'Book Shelf',
+        drawerIcon: ({color, size}) => (
+          <MaterialIcons
+              name= "book"
+              size={size}
+              color={color}
+          />
+        )
+      }}
+      component={BookshelfScreen}    
+    />
+
+    {/* 选项2：书籍管理，对应 app/bookManagement/index.tsx*/}
+    <Drawer.Screen
+      name="bookManagement"
+      options={{
+        title: 'Book Management',
+        drawerIcon: ({color, size}) => (
+          <MaterialIcons
+              name= "library-add"
+              size={size}
+              color={color}
+          />
+        )
+      }}
+      component={BookManagementScreen}
+    />
+
+    <Drawer.Screen
+      name="reader"
+      component={ReaderPage}
+      options={{
+        drawerLabel: () => null,  // 不显示
+        title: '',
+        drawerItemStyle: { height: 0 }, // 高度为 0
+      }}
+    />
+
+      
+    {/* 选项?：测试按钮，用于各种测试，目前是会显示成功录入数据的书籍基础信息，对应 app/DatabaseTest/index.tsx*/}
+    {/* 目前是会显示成功录入数据的书籍基础信息，对应 app/DatabaseTest/index.tming*/}
+    <Drawer.Screen
+      name="databaseTest"
+      options={{ 
+        title: 'For Testing',
+        drawerIcon: ({color, size}) => (
+          <MaterialIcons
+              name= "bug-report"
+              size={size}
+              color={color}
+          />
+        )
+      }}
+      component={DatabaseTest}
+    />
+
+    {/* 选项3：设置，@ming已实现*/}
+    <Drawer.Screen
+      name="settings"
+      options={{
+        title: 'Setting',
+        drawerIcon: ({color, size}) => (
+          <MaterialIcons
+              name= "settings"
+              size={size}
+              color={color}
+          />
+        )
+      }}
+      component={SettingsScreen}
+    />
+  </Drawer.Navigator>
+  )
+}
+
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  const paperTheme =
-    colorScheme === 'dark'
-      ? { ...MD3DarkTheme, colors: Colors.dark.colors}
-      : { ...MD3LightTheme, colors: Colors.light.colors };
-
-  const drawerWidth = //Calculate the width of the drawer based on the screen size
-  (
-    (Dimensions.get('window').width<Dimensions.get('window').height)
-    ? Dimensions.get('window').width
-    : Dimensions.get('window').height
-  )* 8 / 18 + 20; // 这里调侧栏的大小
-  
   const [fontsLoaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -94,120 +192,10 @@ export default function RootLayout() {
     return null;
   }
 
-  
   return (
     <PaperProvider theme={paperTheme}>
       <ReaderProvider>
-        <Drawer.Navigator
-        initialRouteName="bookShelf"
-        //Include the Image
-        drawerContent={(props) => <CustomDrawerContent {...props} />}
-
-        screenOptions={{
-          drawerActiveTintColor: paperTheme.colors.onPrimary,
-          drawerActiveBackgroundColor: paperTheme.colors.primary,
-          drawerInactiveTintColor: paperTheme.colors.onSurfaceVariant,
-          drawerStyle: {
-            // Set the width of the drawer
-            width: drawerWidth,
-            marginHorizontal: 0,
-            left: 0,
-            marginLeft: 0,
-            paddingStart: 0,
-            paddingEnd: 0,
-            backgroundColor: paperTheme.colors.primaryContainer,
-          },
-          drawerItemStyle: {
-            left: 0,
-            marginHorizontal: 0,
-            marginLeft: 0,
-            paddingStart: 0,
-            paddingEnd: 0,
-            width: '150%',
-          },
-          header: () => <Header />, //Override the header that could be turned off in the screen options
-        }}
-      >
-          {/* 选项1：书架，对应 app/(tabs) 目录或文件 */}
-          <Drawer.Screen
-          name="bookShelf"
-          options={{
-            title: 'Book Shelf',
-            drawerIcon: ({color, size}) => (
-              <MaterialIcons
-                 name= "book"
-                 size={size}
-                 color={color}
-              />
-            )
-          }}
-          component={BookshelfScreen}    
-        />
-
-          {/* 选项2：书籍管理，对应 app/bookManagement/index.tsx*/}
-
-          <Drawer.Screen
-          name="bookManagement"
-          options={{
-            title: 'Book Management',
-            drawerIcon: ({color, size}) => (
-              <MaterialIcons
-                 name= "library-add"
-                 size={size}
-                 color={color}
-              />
-            )
-          }}
-          component={BookManagementScreen}
-        />
-
-          <Drawer.Screen
-            name="reader"
-            component={ReaderPage}
-            options={{
-              drawerLabel: () => null,  // 不显示
-              title: '',
-              drawerItemStyle: { height: 0 }, // 高度为 0
-            }}
-          />
-
-          
-          {/* 选项?：测试按钮，用于各种测试，目前是会显示成功录入数据的书籍基础信息，对应 app/DatabaseTest/index.tsx*/}
-          {/* 目前是会显示成功录入数据的书籍基础信息，对应 app/DatabaseTest/index.tming*/}
-          <Drawer.Screen
-            name="databaseTest"
-            options={{ 
-              title: 'For Testing',
-              drawerIcon: ({color, size}) => (
-                <MaterialIcons
-                   name= "bug-report"
-                   size={size}
-                   color={color}
-                />
-              )
-            }}
-            component={DatabaseTest}
-          />
-
-        
-
-          {/* 选项3：设置，@ming已实现*/}
-          <Drawer.Screen
-          name="settings"
-          options={{
-            title: 'Setting',
-            drawerIcon: ({color, size}) => (
-              <MaterialIcons
-                 name= "settings"
-                 size={size}
-                 color={color}
-              />
-            )
-          }}
-          component={SettingsScreen}
-        />
-
-        </Drawer.Navigator>
+        <LeftDrawer />
       </ReaderProvider>
     </PaperProvider>
   );
--- components/CustomDrawerContent.tsx
+++ components/CustomDrawerContent.tsx
@@ -17,26 +17,33 @@ const drawerWidth =
     (Dimensions.get('window').width<Dimensions.get('window').height)
     ? Dimensions.get('window').width
     : Dimensions.get('window').height
-  )* 8 / 18;
+  )* 8 / 18+20;
+
 
 export function CustomDrawerContent(props: DrawerContentComponentProps) {
   return (
     <DrawerContentScrollView {...props} contentContainerStyle={styles.container}>
-      <View style={styles.headerContainer}>
-        <Image
-          source={{ uri: 'https://pic1.imgdb.cn/item/67d09faa066befcec6e366fe.png' }}
-          style={styles.headerImage}
-        />
-      </View>
-
+      <DrawerCover />
       <DrawerItemList {...props} />
     </DrawerContentScrollView>
+  );
+}
+// Will become the cover of latest book
+function DrawerCover() {
+  return (
+    <Image
+      source={{ uri: 'https://pic1.imgdb.cn/item/67d09faa066befcec6e366fe.png' }}
+      style={styles.headerImage}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
+    paddingStart: 1,
+    paddingEnd: 1,
+    paddingTop: 0,
   },
   headerContainer: {
     backgroundColor: '#f0f0f0',
@@ -46,7 +53,10 @@ const styles = StyleSheet.create({
     margin : 0,
   },
   headerImage: {
-    flex : 1,
+    height: drawerWidth*18/13,
+    width: drawerWidth,
+    padding : 0,
+    margin : 0,
     resizeMode: 'cover',
   },
 });
