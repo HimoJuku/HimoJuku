6b615eab6c45110c5902fae8ba4f73f64806182e
小火 <49596040+xiaohuo-desu@users.noreply.github.com>
2025-04-26 00:11:10 +0800

    feat: Add search type functionality and enhance search screen layout

--- app/search.tsx
+++ app/search.tsx
@@ -3,16 +3,19 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import Search from '@/functions/searchBooks';
 import Book from '@/db/models/books';
 import React, { useState, useEffect } from 'react';
-import { ActivityIndicator, Text } from 'react-native-paper';
-import { Card, Paragraph } from 'react-native-paper';
-import { useLocalSearchParams } from 'expo-router';
+import { ActivityIndicator, Text, Card, Paragraph, Chip, Button} from 'react-native-paper';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { View, StyleSheet, FlatList, RefreshControl, Image } from 'react-native';
+import { searchBy } from '@/constants/search';
 
 export default function SearchScreen() {
     const { query } = useLocalSearchParams();
     const [books, setBooks] = useState<Book[]>([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
-
+    const [searchType, setSearchType] = useState<searchBy>({ shown: 'All' });
+    const router = useRouter();
+    
     useEffect(() => {
         if (query) {
             handleSearch();
@@ -37,19 +40,150 @@ export default function SearchScreen() {
         }
     };
 
+      //Open reader method
+    const openReader = (path: string) => {
+        router.push(
+        {
+            pathname: '/reader',
+            params: {
+            path: path
+            }
+        }
+        );
+    };
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
-            <Text style={{ fontSize: 24, margin: 16 }}>Search Results for "{query}"</Text>
             {loading && <ActivityIndicator size="large" color="#0000ff" />}
             {error && <Text>{error}</Text>}
-            {books.map((book) => (
-                <Card key={book.id}>
-                    <Card.Title title={book.title} subtitle={book.author} />
-                    <Card.Content>
-                        <Paragraph>{book.description}</Paragraph>
-                    </Card.Content>
-                </Card>
-            ))}
+            <View style={styles.chipContainer}>
+                <View style={styles.chipRowContainer}>
+                    <Chip onPress={() => console.log('Pressed')} style={styles.chip} >
+                        <Text style={styles.chipText}>All</Text>
+                    </Chip>
+                </View>
+                <View style={styles.chipRowContainer}>
+                    <Chip onPress={() => console.log('Pressed')} style={styles.chip}>
+                        <Text style={styles.chipText}>Title</Text>
+                    </Chip>
+                </View>
+                <View style={styles.chipRowContainer}>
+                    <Chip onPress={() => console.log('Pressed')} style={styles.chip}>
+                        <Text style={styles.chipText}>Author</Text>
+                    </Chip>
+                </View>
+            </View>
+            <View style={styles.titleContainer}>
+                <Button icon="arrow-right" mode="text" onPress={() => console.log('Pressed')}contentStyle={styles.title} labelStyle={{ fontSize: 20, verticalAlign:"bottom"}} >
+                    <Text variant='headlineSmall'>With Title</Text>
+                </Button>
+                <View style={styles.titleRowContainer}>
+                    <FlatList style={{ flexDirection: 'row',flex: 1}}
+                    data={books}
+                    keyExtractor={(b,) => b.id}
+                    refreshControl={<RefreshControl refreshing={loading} />}
+                    renderItem={({ item }) => (
+                        <Card
+                        elevation={0}
+                        onPress={() => openReader(item.filePath)}
+                        >
+                        <Card.Title
+                        style= {{
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        maxWidth: 170,
+                        marginTop: 15,
+                        alignSelf: 'center',
+                        }}
+                        title={item.title}
+                        titleNumberOfLines={2}
+                        subtitle= {item.author}
+                        left={() => (
+                        <Image
+                        style={[styles.image]}
+                        source={
+                            item.coverUrl
+                                ? { uri: item.coverUrl }
+                                : require('@/assets/images/cover-placeholder.png')
+                            }
+                            />
+                        )}
+                        leftStyle ={styles.leftStyle}
+                        />
+                        </Card>
+                        )}
+                        />
+                </View>
+            </View>
+            <View style={styles.authorContainer}>
+            <Button icon="arrow-right" mode="text" onPress={() => console.log('Pressed')}contentStyle={styles.title} labelStyle={{ fontSize: 20, verticalAlign:"bottom"}} >
+                    <Text variant='headlineSmall'>With Author</Text>
+                </Button>
+                </View>
+            <View style={styles.authorRowContainer}>
+                    <Text style={{ fontSize: 22, margin: 16 }}>Search Results for "{query}"</Text>
+                        {books.map((book) => (
+                        <Card key={book.id}>
+                            <Card.Title title={book.title} subtitle={book.author} />
+                            <Card.Content>
+                                <Paragraph>{book.description}</Paragraph>
+                            </Card.Content>
+                        </Card>
+                    ))}
+                </View>
         </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    chipContainer: {
+        alignContent: 'center',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10
+    },
+    chipRowContainer: {
+        paddingEnd: 10,
+    },
+    chip: {
+        borderRadius: 20
+    },
+    chipText: {
+        fontSize: 16,
+        padding: 5,
+        paddingInline: 10,
+        },
+    titleContainer:{
+        flex: 1,
+        justifyContent: 'flex-start'
+    },
+    title:{
+        flexDirection: 'row-reverse',
+        justifyContent: 'flex-end',
+        height: 50,
+    },
+    titleRowContainer:{
+        flex: 1
+    },
+    image:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: "100%",
+        width: "100%",
+
+    },
+    leftStyle:{
+        aspectRatio: 0.72,
+        borderRadius: 4,
+        width: 80,
+        height: 200,
+    },
+    authorContainer:{
+        flex: 0,
+        justifyContent: 'flex-start'
+    },
+    authorRowContainer:{
+        flex: 1
+    },
+})
