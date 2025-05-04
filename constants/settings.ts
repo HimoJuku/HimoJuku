9c26f725f62f5b38715e3243d83c36e7f1aaf905
小火 <49596040+xiaohuo-desu@users.noreply.github.com>
2025-05-04 17:06:59 +0800

    feat: setting panel

--- constants/settings.ts
+++ constants/settings.ts
@@ -15,3 +15,23 @@ export const ThemeContext = React.createContext<ThemeContextType>({
   resolvedTheme: "light",
   setThemePreference: () => {},
 });
+
+export const typesettingDirections = {
+  horizontal: "horizontal",
+  vertical: "vertical",
+};
+
+export const readingDirections = {
+  ltr: "left-to-right",
+  rtl: "right-to-left",
+  ttb: "top-to-bottom",
+  btt: "bottom-to-top"
+};
+
+export const readerMode = {
+  singlePage: "single",
+  doublePage: "double",
+  scroll: "scroll",
+  spread: "spread",
+  continuous: "continuous",
+};
